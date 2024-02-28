192b315ab9cebcc40d2755d3bfe3e3b858760019
Bhanu <bhanusngh12345@gmail.com>
2024-02-29 00:04:12 +0530

    Mobile Navbar done

--- src/components/shared/MobileNavbar.tsx
+++ src/components/shared/MobileNavbar.tsx
@@ -8,6 +8,8 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { AlignRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 const MobileNavbar = () => {
   return (
@@ -18,10 +20,44 @@ const MobileNavbar = () => {
         </SheetTrigger>
         <SheetContent className="bg-white">
           <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
+            <SheetTitle>
+                <Link href={"/"} className="flex gap-2 items-center">
+                    <Image src={"/icons/money.png"} height={30} width={30} alt="logo" />
+                    <p>udhari.com</p>
+                </Link>
+            </SheetTitle>
+            <SheetDescription className="flex flex-col py-6 gap-2">
+                <Link href={"/"} className='mobile_navbar_btn'>
+                    <p>Home</p>
+                </Link>
+
+                <Link href={"/product"} className='mobile_navbar_btn'>
+                    <p>Product</p>
+                </Link>
+
+                <Link href={"/contact-us"} className='mobile_navbar_btn'>
+                    <p>Contact-us</p>
+                </Link>
+
+                <Link href={"/about"} className='mobile_navbar_btn'>
+                    <p>About</p>
+                </Link>
+
+                <Link href={"/sign-in"} className='mobile_navbar_btn'>
+                    <p>sign-in</p>
+                </Link>
+
+                <Link href={"/log-in"} className='mobile_navbar_btn'>
+                    <p>log-in</p>
+                </Link>
+
+                <Link href={"/log-out"} className='mobile_navbar_btn'>
+                    <p>log-out</p>
+                </Link>
+                
+                <Link href={"/profile"} className='mobile_navbar_btn'>
+                    <p>profile</p>
+                </Link>
             </SheetDescription>
           </SheetHeader>
         </SheetContent>
